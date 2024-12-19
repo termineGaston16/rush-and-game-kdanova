84324e420b3a4d82d5ba3d08e4f8948d4755dd98
kda/nova <gastontermine16@gmail.com>
2024-12-19 15:21:46 -0300

    Juego de unir los cables
    Debo aprender como integrar canvas e integrar la logica de que cable está conectad

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import JoinTheCables from './GAMES/JoinTheCables';
 
 export default function App() {
-    return (<BrowserRouter>
-        <Routes>
-            <Route path='*' element='ERROR 404' />
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='*' element={'Error 404'} />
 
-            <Route path='/' element='main' />
-        </Routes>
-    </BrowserRouter>)
+                <Route path='/' element={<JoinTheCables />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
