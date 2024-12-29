d2f026e0f6aa77ee06c77a4a72818986ee8d119d
kda/nova <gastontermine16@gmail.com>
2024-12-29 13:42:32 -0300

    JUEGO DE ATRAPAR A IKO TERMINADO!

--- src/GAMES/CATCHIKO/CatchIko.tsx
+++ src/GAMES/CATCHIKO/CatchIko.tsx
@@ -17,14 +17,14 @@ export default function CatchIko() {
                     if (entries[0].isIntersecting) {
                         setIkoPosition((prevPosition) => ({
                             x: Math.floor(Math.random() * (document.documentElement.scrollWidth)) - 25,
-                            y: prevPosition.y === 0 ? (window.innerHeight + (window.innerHeight / 2))  : 0,
+                            y: prevPosition.y === 0 ? (window.innerHeight + (window.innerHeight / 2)) : 0,
                         }));
                     }
                 },
                 {
                     root: null,
                     rootMargin: "0px",
-                    threshold: .2,
+                    threshold: 0,
                 }
             );
 
@@ -35,35 +35,18 @@ export default function CatchIko() {
         }
     }, []);
 
-    const handleOnMouseMove=(e: React.MouseEvent<HTMLElement, MouseEvent>)=>{
-        if(flashlightRef.current){
-            const {style} = flashlightRef.current
-            style.background = `radial-gradient(circle at ${(e.clientX / window.innerWidth) * 100}% ${(e.clientY / window.innerHeight) * 100}%, rgba(255, 255, 255, 0.412),rgba(0, 0, 0, 0.900) 20%)`
-        }
-    }
-
     return (
         <main
-            onMouseMove={(e) => handleOnMouseMove(e)}
             ref={flashlightRef}
             className="CatchIko"
-        >   
-            <div 
-
-            className="CatchIko__flashlight"></div>
+        >
             <div
                 className="CatchIko__iko"
                 ref={ikoRef}
                 onClick={() => alert('JUEGO GANADO')}
                 style={{
-                    position: "absolute",
-                    width: "200px",
-                    height: "200px",
-                    border: "2px solid aqua",
-                    borderRadius: "10rem",
                     top: `${ikoPosition.y}px`,
                     left: `${ikoPosition.x}px`,
-                    transition: "1.3s",
                 }}
             ></div>
         </main>
