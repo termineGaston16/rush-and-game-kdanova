84bdc941b155eb1c4e7c33731cd3860f1fd53e94
kda/nova <gastontermine16@gmail.com>
2025-01-08 14:47:49 -0300

    Juego de Atrapar a Iko terminado

--- src/MAIN/Home/Home.tsx
+++ src/MAIN/Home/Home.tsx
@@ -70,7 +70,7 @@ export default function Home() {
 
     const continueGameLogic = (numberRandom: number, timeBeforeLosing: NodeJS.Timeout) => {
 
-        switch (3) {
+        switch (0) {
             case 0:
                 setGameOvercome(true)
                 setTimeout(() => { setGameOvercome(false) }, 3000)
